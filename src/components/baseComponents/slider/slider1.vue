562f78d3df047125c945e0d93e1d44097943e892
pangxiaoqing <pxqnice@163.com>
2018-12-10 23:24:45 +0800

    1210

---
+++ src/components/baseComponents/slider/slider1.vue
@@ -0,0 +1,52 @@
+<template>
+    <section :class="cname">
+        <swiper :options="options" :not-next-tick="options.notNextTick">
+            <swiper-slide v-for="item in items" :key="item.href">
+                <router-link :to="{ name: item.href}">
+                    <img :src="item.src" alt="">
+                </router-link>
+            </swiper-slide>
+            <div v-if="options.pagination" slot="pagination" class="swiper-pagination"/>
+        </swiper>
+    </section>
+</template>
+
+<script>
+import { swiper, swiperSlide } from "vue-awesome-swiper"
+
+export default {
+    components: {
+        swiper,
+        swiperSlide,
+    },
+    props: {
+        cname: {
+            type: String,
+            default: "",
+        },
+        options: {
+            type: Object,
+            default() {
+                return {
+                    autoplay: true,
+                    loop: true,
+                    pagination: {
+                        el: ".swiper-pagination",
+                    },
+                    notNextTick: false,
+                }
+            },
+        },
+        items: {
+            type: Array,
+            default() {
+                return []
+            },
+        },
+    },
+}
+</script>
+
+<style lang="css">
+  @import "~swiper/dist/css/swiper.css";
+</style>
