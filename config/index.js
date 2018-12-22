6605d215a62d7121386724cf178dfa7864f3d195
pangxiaoqing <pxqnice@163.com>
2018-12-23 05:36:10 +0800

    2

--- config/index.js
+++ config/index.js
@@ -45,10 +45,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../nanxijiang/index.html'),
+    index: path.resolve(__dirname, '../dist/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../nanxijiang'),
+    assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
